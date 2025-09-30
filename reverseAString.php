fbfaa4b0cd3baea2f42e24e560be45b10695a0ae
AdamKourchi <9orachiadam@gmail.com>
2025-09-30 10:30:26 +0100

    ReverseAString

---
+++ reverseAString.php
@@ -0,0 +1,14 @@
+<?php
+function reverseString(string $string): string
+{
+
+    $arrayString = str_split($string);
+
+    $reversed = array_reverse($arrayString);
+
+    return implode($reversed);
+}
+
+
+
+echo reverseString("Hello");
